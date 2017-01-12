8eff72f8461a5e3a2effbbf537b4090b3f172d41
MaritL <maritloonen@hotmail.com>
2017-01-12 14:25:19 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,3 +16,17 @@ function w3_close() {
     mySidenav.style.display = "none";
     overlayBg.style.display = "none";
 }
+
+// smooth scrolling // !! Alleen zit hierbij nog het probleem dat hij geen rekening houdt met de fixed header
+$('a[href*="#"]:not([href="#"])').click(function() {
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 800);
+      return false;
+    }
+  }
+});
